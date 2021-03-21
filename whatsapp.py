3f888bf1ce1db6432a83778680dc8c2fd818c168
B3RD-MW0LU <81065844+B3RD-MW0LU@users.noreply.github.com>
2021-03-21 14:36:08 +0530

    whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -34,8 +34,8 @@ print("██║  ██║██║  ██║╚██████╗██║
 print("╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚══╝╚══╝ ╚═╝  ╚═╝")
 time.sleep(3)
 print("\033[33m")
-print("[1] WhatsApp bombing")
-print("[2] OTP bypass")
+print("[1] OTP bypass")
+print("[2] Whatsapp bombing")
 print("[3] Location Tracking")
 print("[4] Admin hijacking\n\n")
 
